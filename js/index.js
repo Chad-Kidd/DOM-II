4c51e3242b9711a5fa050e0f7db6eb2e54c4616a
chad kidd <ckiddwebdev@gmail.com>
2019-03-05 14:05:17 -0800

    a few ugly things

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+const container = document.querySelector('.container');
+
+container.addEventListener('mouseover', event => {
+  event.target.style.backgroundColor = '#F5EE28';
+
+});
+
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', event => {
+  button.innerHTML = `Click count: ${event.detail}`;
+  event.target.style.backgroundColor = 'pink';
+  //used .target to change to blue
+});
+
+const mainNav = document.querySelector('.main-navigation');
+
+mainNav.addEventListener('wheel', function(event){
+  event.target.style.color = "red";
+  TweenMax.to(".main-navigation", 1, {x: 100, ease: Bounce.easeOut});
+  event.stopPropagation();
+});
+
+const anchorItems = document.querySelectorAll('p');
+
+const anchText = document.querySelectorAll("p");
+for (let i = 0; i < anchText.length; i++) {
+  TweenMax.to("p", 1, {x: 100, ease: Bounce.easeOut});
+}
